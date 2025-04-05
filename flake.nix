1c0fb6ec7b0e22fa9668fa24a40c832134c2e392
Simon Théorêt <simonteoret@hotmail.com>
2025-04-04 23:11:26 -0400

    build: specified config for enigo (testing)

--- flake.nix
+++ flake.nix
@@ -7,43 +7,30 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    {
-      nixpkgs,
-      rust-overlay,
-      flake-utils,
-      ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
+        pkgs = import nixpkgs { inherit system overlays; };
         rust = pkgs.rust-bin.stable.latest.default.override {
-          extensions = [
-            "rust-src"
-            "rust-analyzer"
-          ];
+          extensions = [ "rust-src" "rust-analyzer" ];
           targets = [ ];
         };
-      in
-      {
+      in {
         devShells.default = pkgs.mkShell {
-          buildInputs =
-            [ rust ]
-            ++ (with pkgs; [
-              pkg-config
-              nixfmt-classic
-              nil
-              shellcheck
-              shfmt
-              nodePackages_latest.bash-language-server
-              cargo-expand
-              xorg.libX11
-            ]);
+          buildInputs = [ rust ] ++ (with pkgs; [
+            pkg-config
+            nixfmt-classic
+            nil
+            shellcheck
+            shfmt
+            nodePackages_latest.bash-language-server
+            cargo-expand
+            xorg.libX11
+            pkg-config
+            xdotool
+          ]);
+          shellHook = "echo ${pkgs.xorg.libX11}";
         };
-      }
-    );
+      });
 }
